9b0f467678a2e07a17aec26d9480f2348b0af27c
JosePereira97 <74066223+JosePereira97@users.noreply.github.com>
2020-11-07 18:33:52 +0000

    1º Função feita

--- AASB.py
+++ AASB.py
@@ -1,4 +1,10 @@
-print(cogumelos)
-print('hello world')
+def tranforma():
+    '''
+    Função resposável por reseber um ficheiro
+    e de dar return de uma sequência em string
+    '''
+    filename = input ("filename: ")
+    my_file = open(filename)
+    my_file = my_file.readline()
+    return(my_file)
 
-def max ()
