b73f7eae9761e2a3cf33d8afb4956a26894041fc
JosePereira97 <74066223+JosePereira97@users.noreply.github.com>
2020-11-09 14:13:18 +0000

    3º função

--- AASB.py
+++ AASB.py
@@ -8,3 +8,12 @@ def tranforma():
     my_file = my_file.readline()
     return(my_file)
 
+def complemento_inverso():
+    '''
+    Função responsável por devolver o 
+    complemento inverso de uma sequência de DNA
+    '''
+    seq = input('Introduza a sequência de DNA: ')
+    seq = seq[::-1].upper()
+    seq = seq.replace('A', 't').replace('T', 'a').replace('C', 'g').replace('G', 'c')
+    return(seq)
