0c418b6b8652da468b3f3cb59d126806f4b18657
pg42862 <74066156+pg42862@users.noreply.github.com>
2020-11-09 15:28:05 +0000

    5º função

--- AASB.py
+++ AASB.py
@@ -25,3 +25,52 @@ def transcricao():
     seq = input ('Introduza a sequência de DNA: ')
     seq_trans = seq.replace('A', 'u').replace('T', 'a').replace('C', 'g').replace('G', 'c').upper()
     return (seq_trans)
+
+def traducao():
+    '''
+    Função responsável pela tradução da cadeia de DNA
+    '''
+    gencode = {
+    'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
+    'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
+    'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K',
+    'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',
+    'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L',
+    'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
+    'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q',
+    'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
+    'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V',
+    'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A',
+    'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E',
+    'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
+    'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S',
+    'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L',
+    'TAC':'Y', 'TAT':'Y', 'TAA':'_', 'TAG':'_',
+    'TGC':'C', 'TGT':'C', 'TGA':'_', 'TGG':'W'}
+
+    cadeiaDNA= input()
+    cadeiaDNA= cadeiaDNA.upper()
+    def tradux(DNA): 
+        import re
+        DNA = re.findall('...', DNA)
+        t = []
+        for y in DNA:
+            t.append(gencode[y])
+        result=''.join(t)
+        return(result)
+
+    for a in range(3):
+        return (tradux(cadeiaDNA[a:]))
+
+traducao()
+
+
+
+
+
+
+
+    
+
+
+    
