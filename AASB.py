1a896cc77114162cda8469498d0a4018376514cc
pg42876 <74066185+pg42876@users.noreply.github.com>
2020-11-09 14:40:37 +0000

    4º função

---
+++ AASB.py
@@ -0,0 +1,27 @@
+def tranforma():
+    '''
+    Função resposável por receber um ficheiro
+    e de dar return de uma sequência em string
+    '''
+    filename = input ("filename: ")
+    my_file = open(filename)
+    my_file = my_file.readline()
+    return(my_file)
+
+def complemento_inverso():
+    '''
+    Função responsável por devolver o 
+    complemento inverso de uma sequência de DNA
+    '''
+    seq = input('Introduza a sequência de DNA: ')
+    seq = seq[::-1].upper()
+    seq = seq.replace('A', 't').replace('T', 'a').replace('C', 'g').replace('G', 'c')
+    return(seq)
+
+def transcricao():
+    '''
+    Função responsável pela transcrição de DNA
+    '''
+    seq = input ('Introduza a sequência de DNA: ')
+    seq_trans = seq.replace('A', 'u').replace('T', 'a').replace('C', 'g').replace('G', 'c').upper()
+    return (seq_trans)
