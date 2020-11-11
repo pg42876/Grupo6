825a7e2814a49c83ab553ef2d80b3c1fde915fb1
pg42876 <74066185+pg42876@users.noreply.github.com>
2020-11-11 10:46:26 +0000

    Função 8

--- AASB.py
+++ AASB.py
@@ -99,7 +99,56 @@ def contar_bases():
             nucleotidos [x]= 0
         nucleotidos[x]+= 1
     return(nucleotidos)
-            
+
+def reading_frames():
+    '''
+    Função que devolve uma lista com as reading frames
+    '''
+    gencode = {
+    'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
+    'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
+    'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K',
+    'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',
+    'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L',
+    'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
+    'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q',
+    'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
+    'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V',
+    'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A',
+    'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E',
+    'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
+    'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S',
+    'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L',
+    'TAC':'Y', 'TAT':'Y', 'TAA':'_', 'TAG':'_',
+    'TGC':'C', 'TGT':'C', 'TGA':'_', 'TGG':'W'}
+
+    def traduz(seq):
+        import re
+        seq = re.findall('...', seq)
+
+        l = []
+
+        for x in seq:
+            l.append(gencode[x])
+
+        result = "".join(l)
+        return (result)
+    
+    seq = input()
+    seq = seq.upper()  
+
+    for a in range(3):
+        return (traduz(seq[a:]))
+
+    seq1 = seq[::-1]
+    seq1 = seq1.replace('A','t').replace('T','a').replace('C','g').replace('G','c').upper()
+
+    for a in range(3):
+        return (traduz(seq1[a:]))
+
+
+
+
 
 
 
