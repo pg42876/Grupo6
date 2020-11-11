e954c7768581de7956eb8e9f8a4ca294089b03e8
LuisMoncaixa96 <74066200+LuisMoncaixa96@users.noreply.github.com>
2020-11-11 11:11:22 +0000

    Update AASB.py

--- AASB.py
+++ AASB.py
@@ -146,7 +146,55 @@ def reading_frames():
     for a in range(3):
         return (traduz(seq1[a:]))
 
+def proteins():
+    '''
+    Função que devolve a lista de todas
+    as proteínas ordenadas por tamanho e 
+    por ordem alfabética para as do mesmo
+    tamanho
+    '''
+    seq = input("Introduza uma sequência: ")
+    seq = seq.upper()
+    import re
+    gencode = {
+            'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
+            'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
+            'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K',
+            'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',
+            'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L',
+            'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
+            'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q',
+            'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
+            'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V',
+            'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A',
+            'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E',
+            'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
+            'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S',
+            'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L',
+            'TAC':'Y', 'TAT':'Y', 'TAA':'_', 'TAG':'_',
+            'TGC':'C', 'TGT':'C', 'TGA':'_', 'TGG':'W'}
 
+    def traducao(seq):
+        proteina = []
+        for i in range(3):
+            traducao = []
+            sequencia = re.findall('...', seq[i:])
+            for x in sequencia:
+                traducao.append(gencode[x])
+            traducao = "".join(traducao)
+            proteina.extend(re.findall('M.*?_', traducao))
+        return(proteina)
+    
+    def customkey(protein):
+        return -len(protein), protein
+    
+    protein = []
+    protein = traducao(seq)
+    
+    protein = list(dict.fromkeys(protein))
+
+    protein = sorted(protein, key = customkey)
+    print("\n".join(protein))
 
 
 
