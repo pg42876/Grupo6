fd7a0b62966fc51ddac2cf9b95d445cffe79ad9f
JosePereira97 <74066223+JosePereira97@users.noreply.github.com>
2020-11-11 17:27:25 +0000

    Revert "merdas"
    This reverts commit 9680dd2672cae3dcd17011aee143305b790cbc89.

--- AASB.py
+++ AASB.py
@@ -46,12 +46,8 @@ def transcricao(seq):
     '''
     Função responsável pela transcrição de DNA
     '''
-    seq = seq.upper()
-    if validarseq(seq)==True:
-        seq_trans = seq.replace('A', 'u').replace('T', 'a').replace('C', 'g').replace('G', 'c').upper()
-        return (seq_trans)
-    else:
-        return('Não é um sequência válida de DNA')
+    seq_trans = seq.replace('A', 'u').replace('T', 'a').replace('C', 'g').replace('G', 'c').upper()
+    return (seq_trans)
 
 def traducao(cadeiaDNA):
     '''
@@ -76,16 +72,17 @@ def traducao(cadeiaDNA):
     'TGC':'C', 'TGT':'C', 'TGA':'_', 'TGG':'W'}
 
     cadeiaDNA= cadeiaDNA.upper()
-    if validarseq(cadeiaDNA)==True:
+    def tradux(DNA): 
         import re
-        DNA = re.findall('...',cadeiaDNA)
+        DNA = re.findall('...', DNA)
         t = []
         for y in DNA:
             t.append(gencode[y])
         result=''.join(t)
         return(result)
-    else:
-        return('Não é uma sequência de DNA')
+
+    for a in range(3):
+        return (tradux(cadeiaDNA[a:]))
 
 def validarseq(seq):
     '''
@@ -103,16 +100,12 @@ def contar_bases(seq):
     Função que conta as bases de uma sequência e
     devolve um dicionário com a contagem
     '''
-    seq= seq.upper()
-    if validarseq(seq)==True:
-        nucleotidos = {}
-        for x in seq:
-            if x not in nucleotidos:
-                nucleotidos [x]= 0
-            nucleotidos[x]+= 1
-        return(nucleotidos)
-    else:
-        return('string apresentada não é uma sequência')
+    nucleotidos = {}
+    for x in seq:
+        if x not in nucleotidos:
+            nucleotidos [x]= 0
+        nucleotidos[x]+= 1
+    return(nucleotidos)
 
 def reading_frames(seq):
     '''
@@ -148,21 +141,17 @@ def reading_frames(seq):
         result = "".join(l)
         return (result)
     
-    seq = seq.upper()
-    if validarseq(seq)==True:
-        seq1 = seq [::-1]
-        seq1 = seq1.replace('A','t').replace('T','a').replace('C','g').replace('G','c').upper()
-        l2 = []
-        for x in range(3):
-            l2.eppend(traduz(seq1[x:]))
-    
-        l1 = []
-        for a in range(3):
-            l1.append(traduz(seq[a:]))
-        l3 = []
-        l3 = l2 + l1
-        return(l3)
-    
+    seq = seq.upper()  
+
+    for a in range(3):
+        return (traduz(seq[a:]))
+
+    seq1 = seq[::-1]
+    seq1 = seq1.replace('A','t').replace('T','a').replace('C','g').replace('G','c').upper()
+
+    for a in range(3):
+        return (traduz(seq1[a:]))
+
 def proteins(seq):
     '''
     Função que devolve a lista de todas
@@ -171,9 +160,8 @@ def proteins(seq):
     tamanho
     '''
     seq = seq.upper()
-    if validarseq(seq)== True:
-        import re
-        gencode = {
+    import re
+    gencode = {
             'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
             'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
             'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K',
@@ -188,30 +176,45 @@ def proteins(seq):
             'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
             'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S',
             'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L',
-            'TAC':'Y', 'TAT':'Y', 'TAA':'', 'TAG':'',
+            'TAC':'Y', 'TAT':'Y', 'TAA':'_', 'TAG':'_',
             'TGC':'C', 'TGT':'C', 'TGA':'_', 'TGG':'W'}
 
-        def traducao(seq):
-            proteina = []
-            for i in range(3):
-                traducao = []
-                sequencia = re.findall('...', seq[i:])
-                for x in sequencia:
-                    traducao.append(gencode[x])
-                    traducao = "".join(traducao)
-                proteina.extend(re.findall('M.*?_', traducao))
-            return(proteina)
+    def traducao(seq):
+        proteina = []
+        for i in range(3):
+            traducao = []
+            sequencia = re.findall('...', seq[i:])
+            for x in sequencia:
+                traducao.append(gencode[x])
+            traducao = "".join(traducao)
+            proteina.extend(re.findall('M.*?_', traducao))
+        return(proteina)
     
-        def customkey(protein):
-            return -len(protein), protein
+    def customkey(protein):
+        return -len(protein), protein
     
-        protein = []
-        protein = traducao(seq)
+    protein = []
+    protein = traducao(seq)
     
-        protein = list(dict.fromkeys(protein))
+    protein = list(dict.fromkeys(protein))
 
-        protein = sorted(protein, key = customkey)
-        return(protein)
-    else:
-        return('seq inexistente')
+    protein = sorted(protein, key = customkey)
+    print("\n".join(protein))
+    
+
+
+    
+
+
+        
+
+
+
+
+
+
+
+    
+
+
     
